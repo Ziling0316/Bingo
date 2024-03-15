aede87e0271022cbea4435ecb39c12b4acab0b44
lazybear-0425 <wendy930425@gmail.com>
2024-03-15 23:30:39 +0800

    finish_1

--- bingo.py
+++ bingo.py
@@ -12,16 +12,20 @@ def start():
     os.system('clear')
     print('Welcome to '+Fore.RED+'Bingo Game!')
     print('Please input ', end='')
-    print(Back.MAGENTA+Fore.BLACK+'name;your number', end='')
+    print(Back.MAGENTA+Fore.BLACK+'name,your number', end='')
     print('to join this game')
     print('Your input = ', end='')
+#    text = input()
+#    name = text.split(',')[0]
+#    num = text.split(',')[1:]
     text = input()
-    name, num = text.split(';')
+    name = text.split(';')[0]
+    num = text.split(';')[1].split(',')
     os.system("clear")
     print('Welcome '+Fore.YELLOW+name, end='')
     print(" ! Let's wait for the game start!")
     print("This is your bingo card")
-    num = num.split(',')
+#    num = num.split(',')
     for i in range(0, len(num)):
         if (i+1)%5!=0:
             print('{:>4}'.format(num[i]), end='')
@@ -50,8 +54,7 @@ def check_bingo(bingo_card):
 
 def server(network, port, ser_addr):
     num = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-    player = {}
-    # need remember IP address & port ?
+    player = {} # record {address:[name,number]}
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.bind((ser_addr, port)) #flag
@@ -61,18 +64,21 @@ def server(network, port, ser_addr):
         i = 1
         while(1):
             data, address = sock.recvfrom(BUFSIZE)
+#            text = data.decode('ascii').split(',')
+#            who = text[0]
+#            number = text[1:]
             text = data.decode('ascii').split(';')
             who = text[0]
             number = text[1].split(',')
             player[address] = [who,number]
-            print(f'Player {who} add this game.')
+            print(f'Player \033[33m{who}\033[0m join this game.')
 
             text = f'You are player \033[33m{i}\033[0m, Welcome! The game is expected to start in {delay} seconds'
             text = text.encode('UTF-8')
             sock.sendto(text,address)
             i += 1
     except:
-        print('Game Start!')
+        print('\033[92mGame Start!\033[0m')
     text = 'start'
     sock.sendto(text.encode('ascii'), (network, port))
     delay = 2.0 #flag
@@ -85,6 +91,8 @@ def server(network, port, ser_addr):
         print(f'Number : \033[36m{n}\033[0m') #flag
         num[num.index(n)] = 'X' # == ? #flag
         text = str(n)
+        sock.sendto(text.encode('ascii'), (network, port))
+
         for x in player:
            player[x][1][player[x][1].index(str(n))] = 'X'
 
@@ -99,21 +107,23 @@ def server(network, port, ser_addr):
                     che = check_bingo(player[address][1])
                     if che : 
                         nobody_win = 0;
-                        text = 'You win!'
-                        print(f'Winner is \033[30;47m{player[address][0]}\033[0m')
-                        sock.sendto(text.decode(),address)
+#                        text = 'You win!'
+#                        sock.sendto(text.decode(),address)
 
-                        text = 'Winner is {player[address][0]}'
+                        print(f'Winner is \033[30;47m{player[address][0]}\033[0m')
+
+                        text = f'Bingo, {player[address][0]} from {address[0]}'
                         sock.sendto(text.decode(),(network,address)) #broadcast
                         break;
                     else:
                         text = 'You don\'t win!:('
                         sock.sendto(text.decode(),address)
         except:
-            print('\033[90mNobody bingo\033[0m')
-
-        sock.sendto(text.encode('ascii'), (network, port))
-    print('Game End!')
+            if nobody_win:
+                print('\033[90mNobody bingo\033[0m')
+        if nobody_win == 0:
+            sock.sendto(text.encode('ascii'), (network, port))
+    print('\033[92mGame End!\033[0m')
     #indata, addr = sock.recvfrom(BUFSIZE)
     #print('recvfrom client({}):{} '.format(addr, indata.decode()))
 
@@ -141,11 +151,12 @@ def client(interface, port, ser_addr):
     print("Let's start the game!")
     while flag==False:
         indata, addr = sock.recvfrom(BUFSIZE)
-        if indata.decode('ascii')[:6] == 'Winner':
+        if indata.decode('ascii')[:5] == 'Bingo':
             print(indata.decode('ascii'))
-            break;
+            return
+#            break;
 
-        os.system('clear')
+#        os.system('clear')
         print('recvfrom server({}):\033[36m{}\033[0m'.format(addr, indata.decode()))
         if nums[nums.index(indata.decode())] == 'X' or indata.decode() == 'start':
             continue;
@@ -170,8 +181,8 @@ def client(interface, port, ser_addr):
     while 1:
         sock.sendto(text.encode(),(ser_addr, port))
         indata, addr = sock.recvfrom(BUFSIZE)
-        if indata.decode() == 'You win!':
-            print(indata.decode('ascii'))
+        if indata.decode()[:5] == 'Bingo':
+            print('You Win!')
             break;
 
 if __name__ == '__main__':
