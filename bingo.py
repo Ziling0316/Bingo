2c71f0323281fb8701fc67c6fcfbecbcc25e03eb
Ziling <104045895+Ziling0316@users.noreply.github.com>
2024-09-16 16:40:33 +0800

    Update bingo.py

--- bingo.py
+++ bingo.py
@@ -4,20 +4,21 @@ import os
 from time import sleep
 from random import choice, shuffle
 import argparse, socket
+import zmq
 BUFSIZE = 65535
 tmp_port = 9989
 
 def test_bingo_table(num: list):
     bingo_table = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
     if(len(num)!=25):
-        #If the length passed by the client is less than 25,
+        #If the length passed by the client is less than 25, 
         #the Bingo table will be randomly generated for the client.
         shuffle(bingo_table)
         print("Invalid length")
         return bingo_table
 
     elif(len(set(num))!=len(num)):
-        #If the Bingo table passed by the client has repeated numbers,
+        #If the Bingo table passed by the client has repeated numbers, 
         #a new Bingo table will be randomly generated for the client.
         shuffle(bingo_table)
         print("Repeat numbers")
@@ -73,41 +74,42 @@ def check_bingo(bingo_card):
     return False
 
 def server(network, port, ser_addr):
+    url = "tcp://{}:{}".format(ser_addr, port)
     num = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-    player = {}
-    # record {address:[name,number]}
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    sock.bind((ser_addr, port))
+    player = {} 
+    context = zmq.Context()
+    sock = context.socket(zmq.PUB)
+    sock.bind(url) 
+    Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    Sock.bind((ser_addr, port))
     delay = 20.0
     # The waiting time for the client to join is 20 seconds
-    sock.settimeout(delay)
+    Sock.settimeout(delay)
     try:
         text = ''
         user_count = 1
         while(1):
-            data, address = sock.recvfrom(BUFSIZE)
-            data = data.decode('ascii')
-            who = data
-            data, address = sock.recvfrom(BUFSIZE)
+            name, address = Sock.recvfrom(BUFSIZE)
+            name = name.decode('ascii')
+            data, address = Sock.recvfrom(BUFSIZE)
             data = data.decode('ascii')
             number = [i.strip() for i in data[1:-1].split(',')]
-            player[address] = [who,number]
-            print(f'Player \033[33m{who}\033[0m join this game.')
+            player[address] = [name, number]
+            print(f'Player \033[33m{name}\033[0m join this game.')
 
             text = f'You are player \033[33m{user_count}\033[0m, Welcome! The game is expected to start in \033[33m{delay}\033[0m seconds'
-            text = text.encode('ascii')
-            sock.sendto(text,address)
+            Sock.sendto(text.encode('ascii'),address)
             user_count += 1
     except socket.timeout:
         if user_count == 1:
             print('Nobody join this game')
             sock.close()
+            Sock.close()
         else:
             print('\033[92mGame Start!\033[0m')
             text = 'start'
-            sock.sendto(text.encode('ascii'), (network, port))
-    delay = 2.0
+            sock.send_string(text)
+    delay = 2.0 
     #The waiting time for the client to Bingo is 2 seconds.
     nobody_win = 1
     while nobody_win:
@@ -117,16 +119,15 @@ def server(network, port, ser_addr):
         print(f'Number : \033[36m{n}\033[0m') #flag
         num[num.index(n)] = 'X' # == ? #flag
         text = str(n)
-        sock.sendto(text.encode('ascii'), (network, port))
-
+        sock.send_string(text)
         for x in player:
            player[x][1][player[x][1].index(str(n))] = 'X'
         # Bingo table backup of client
-        sock.settimeout(delay)
+        Sock.settimeout(delay)
         try:
             print('\033[90mReceving Bingo...\033[0m')
             while True:
-                data, address = sock.recvfrom(BUFSIZE)
+                data, address = Sock.recvfrom(BUFSIZE)
                 data = data.decode('ascii')
                 che = 0
                 if(data == 'Bingo'):
@@ -134,20 +135,21 @@ def server(network, port, ser_addr):
                     if che :
                         nobody_win = 0
                         print(f'Winner is \033[30;47m{player[address][0]}\033[0m')
-                        sock.sendto('Bingo'.encode(), address)
-                        text = f'Bingo, {player[address][0]} from {address[0]}'
-                        sock.sendto(text.encode(), (network, port)) #broadcast
+                        Sock.sendto('Bingo'.encode(), address)
+                        text = f'Bingo, {player[address][0]} from {address}'
+                        sock.send_string(text) #broadcast
                         break
                     else:
                         text = 'You are wrong!:('
-                        sock.sendto(text.decode(),address)
+                        Sock.sendto(text.encode(),address)
         except socket.timeout:
             if nobody_win:
                 print('\033[90mNobody bingo\033[0m')
             else:
                 text = 'Game End!'
-                sock.sendto(text.encode('ascii'), (network, port))
+                sock.send_string(text)
                 sock.close()
+                Sock.close()
 
 
 def client(interface, port, ser_addr):
@@ -155,31 +157,37 @@ def client(interface, port, ser_addr):
     Bingo_Flag = False
     name = In[0]
     nums = In[1]
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((interface, port))
-
+    url = "tcp://{}:{}".format(ser_addr, port)
+    context = zmq.Context()
+    sock = context.socket(zmq.SUB)
+    sock.setsockopt_string(zmq.SUBSCRIBE,'')
+    sock.connect(url)
+    
+    Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    Sock.bind((interface, port))
     text = name.encode('ascii') #flag
-    sock.sendto(text,(ser_addr, port))
+    Sock.sendto(text,(ser_addr, port))
     text = str(nums).encode('ascii') #flag
-    sock.sendto(text,(ser_addr, port))
-
-    while True:
-        indata, addr = sock.recvfrom(BUFSIZE)
-        print(indata.decode('ascii'))
-        if indata.decode()=='start':
+    Sock.sendto(text,(ser_addr, port))
+   
+    indata, address = Sock.recvfrom(BUFSIZE)
+    print(indata.decode('ascii'))
+    indata = sock.recv_string()
+    while(1):
+        if indata == 'start':
+            os.system('clear')
+            print("Let's start the game!")
             break
-    os.system('clear')
-    print("Let's start the game!")
     while Bingo_Flag == False:
-        indata, addr = sock.recvfrom(BUFSIZE)
-        if indata.decode('ascii')[:5] == 'Bingo':
-            print(indata.decode('ascii'))
-            exit()
-        print('recvfrom server({}):\033[36m{}\033[0m'.format(addr, indata.decode()))
-        if nums[nums.index(int(indata.decode()))] == 'X' or indata.decode() == 'start':
+        indata = sock.recv_string()
+        if indata[0:5]=="Bingo":
+            print('recvfrom server({}):'.format(address), indata)
+            break
+        print('recvfrom server({}):\033[36m{}\033[0m'.format(address, indata))
+        if nums[nums.index(int(indata))] == 'X' or indata == 'start':
             continue
         else:
-            nums[nums.index(int(indata.decode()))]='X'
+            nums[nums.index(int(indata))]='X'
             for i in range(0, len(nums)):
                 print_red = ''
                 print_end = ''
@@ -197,8 +205,8 @@ def client(interface, port, ser_addr):
             if Bingo_Flag == True:
                 text = 'Bingo'
                 print(Fore.YELLOW+"Bingo!")
-                sock.sendto(text.encode(),(ser_addr, port))
-                indata, addr = sock.recvfrom(BUFSIZE)
+                Sock.sendto(text.encode(),(ser_addr, port))
+                indata, addr = Sock.recvfrom(BUFSIZE)
                 if indata.decode()[:5] == 'Bingo':
                     print('You Win!')
                     break
